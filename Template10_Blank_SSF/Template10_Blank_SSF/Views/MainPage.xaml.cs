a3cf6525268c4f3f4b16540038ff91d456bea7aa
Sean Fulcher <sfulch01@baker.edu>
2016-05-14 17:55:45 -0400

    InitialPushOfAll3Projects

---
+++ Template10_Blank_SSF/Template10_Blank_SSF/Views/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Template10_Blank_SSF.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
---
+++ Template10_Minimal_SSF/Template10_Minimal_SSF/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Template10_Minimal_SSF.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Template10_Minimal_SSF.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
